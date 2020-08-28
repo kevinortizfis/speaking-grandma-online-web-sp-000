b993051d0cf39b1dbcd2dc1a1f4e9aef321c9448
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-28 00:52:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(speech)
   if speech == speech.upcase
     "I LOVE YOU TOO PUMPKIN!"
+  else "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
