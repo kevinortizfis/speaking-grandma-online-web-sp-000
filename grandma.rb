688843d5f182f5ca04e30de9340de338450cc485
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-28 00:54:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speech)
   until speech == speech.upcase
-    "I LOVE YOU TOO PUMPKIN!"
-  else "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
